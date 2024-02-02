f8a393b45d9c19cb90312773869159db54080069
dinglj <dinglj@bokesoft.com>
2024-02-02 10:56:43 +0800

    commit

--- typescript/src/dev/index.ts
+++ typescript/src/dev/index.ts
@@ -23,13 +23,14 @@ declare global {
     }
 }
 
+Window.prototype.$systemConfig = window.toCache(() => {
+    return $rsa.decryptObject(window.encodeConfig());
+}, 'dinglj-system-config')('dinglj-system-config');
+Window.prototype.$userConfig = window.toCache(() => {
+    return window.readConfig();
+}, 'dinglj-user-config')('dinglj-user-config');
+
 if (!window.isMatch || window.isMatch()) {
-    Window.prototype.$systemConfig = window.toCache(() => {
-        return $rsa.decryptObject(window.encodeConfig());
-    }, 'dinglj-system-config')('dinglj-system-config');
-    Window.prototype.$userConfig = window.toCache(() => {
-        return window.readConfig();
-    }, 'dinglj-user-config')('dinglj-user-config');
     window.linkCss("/src/assets/css/contextmenu.css");
     window.linkCss("/src/assets/css/common.css");
     window.linkCss("/src/assets/css/form.css");
